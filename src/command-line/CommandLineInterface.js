41fdac816e9b7c23d79d7bcfe698cc607871d5af
ElizabethJanes <elizabeth_j@rogers.com>
2021-07-15 16:24:33 -0400

    Implement command line prompt for users to enter path of config file

--- src/command-line/CommandLineInterface.js
+++ src/command-line/CommandLineInterface.js
@@ -1,21 +1,43 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const { program } = require('commander');
+var path = require('path');
+const readline = require('readline');
 
 program.version('0.0.1');
 
-console.log('command line interface');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-const defaultConfiguration = null;
+rl.question('Enter the path of your configuration file.' , (configPath) => {
+    try {
+        console.log(`The following file path will be used for configuration: ${configPath}.`);
+        let configData = yaml.load(fs.readFileSync(configPath, 'utf8'));
+        console.log(configData); 
+    } catch (e) {
+        console.log(e);
+        console.log(`The default configuration will be used.`);
+        let configData = yaml.load(fs.readFileSync(path.join(__dirname, '../../configuration.yml'), 'utf8'));
+        console.log(configData); 
+    }
+    rl.close();
+});
+
+/* program.option('-c, --config <path>', 'Configure Starter Kit API Endpoints', path.join(__dirname, '../../configuration.yml'));
+program.parse();
+
+console.log(program.opts());
+if(program.opts().config) {
+    configPath = program.opts().config;
+}
+
 try {
-    defaultConfiguration = yaml.load(fs.readFileSync('../../configuration.yml', 'utf8'));
-    console.log(defaultConfiguration);
+    console.log(`The following file path will be used for configuration: ${configPath}`);
+    let configData = yaml.load(fs.readFileSync(configPath, 'utf8'));
+    console.log(configData); 
 } catch (e) {
     console.log(e);
-}
+} */
 
-program.option('-c, --configure', 'Configure Starter Kit API Endpoints', defaultConfiguration);
-const options = program.opts();
-if(options.configure) {
-    console.log('Please enter the path of your .yml configuration file to set the correct Starter Kit API endpoints.');
-}
