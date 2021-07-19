d36850c2b4e7d8845e0d99917c4edb96635a38c2
ElizabethJanes <elizabeth_j@rogers.com>
2021-07-19 11:45:19 -0400

    Update command line interface and update Home page UI to display configurable services

--- src/command-line/CommandLineInterface.js
+++ src/command-line/CommandLineInterface.js
@@ -1,43 +1,35 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
-const { program } = require('commander');
 var path = require('path');
 const readline = require('readline');
-
-program.version('0.0.1');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Enter the path of your configuration file.' , (configPath) => {
+let configPath = null;
+let configData = null;
+
+rl.question('Enter the path of your configuration file or type "default" to choose the default configuration: ', (userEntry) => {
     try {
-        console.log(`The following file path will be used for configuration: ${configPath}.`);
-        let configData = yaml.load(fs.readFileSync(configPath, 'utf8'));
+        if(userEntry == "default") {
+            configPath = path.join(__dirname, '../../configuration.yml');
+            console.log(`The default configuration will be used.`)
+        }
+        else {
+            configPath = userEntry;
+            console.log(`The following file path will be used for configuration: ${configPath}.`);
+        }
+        configData = yaml.load(fs.readFileSync(configPath, 'utf8'));
         console.log(configData); 
     } catch (e) {
         console.log(e);
-        console.log(`The default configuration will be used.`);
-        let configData = yaml.load(fs.readFileSync(path.join(__dirname, '../../configuration.yml'), 'utf8'));
+        console.log(`The path you provided is invalid. The default configuration will be used instead.`);
+        configData = yaml.load(fs.readFileSync(path.join(__dirname, '../../configuration.yml'), 'utf8'));
         console.log(configData); 
     }
     rl.close();
 });
 
-/* program.option('-c, --config <path>', 'Configure Starter Kit API Endpoints', path.join(__dirname, '../../configuration.yml'));
-program.parse();
-
-console.log(program.opts());
-if(program.opts().config) {
-    configPath = program.opts().config;
-}
-
-try {
-    console.log(`The following file path will be used for configuration: ${configPath}`);
-    let configData = yaml.load(fs.readFileSync(configPath, 'utf8'));
-    console.log(configData); 
-} catch (e) {
-    console.log(e);
-} */
-
+exports.configuration = configData;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,8 +6,49 @@ import {
 import { 
   Container, 
   Button, 
-  Typography
+  Typography, 
+  Accordion, 
+  AccordionSummary, 
+  AccordionDetails, 
+  List, 
+  ListItem, 
+  Box
  } from '@material-ui/core';
+ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+ //temporary configuration object
+ let configuration = {
+    services: [
+      {
+        drs: [
+          {admin_endpoint: 'http://localhost:8080/admin/ga4gh/drs/v1/'}, 
+          {public_endpoint: 'http://localhost:8080/ga4gh/drs/v1/'}
+        ]
+      }
+    ]
+  }
+
+const AccordionData = (props) => {
+  let starterKitService = configuration.services[0][props.service];
+  let accordion = null;
+  starterKitService 
+  ? 
+  accordion = starterKitService.map((service, index) => {
+    return(
+      <ListItem key={`${Object.keys(service)}_${Object.values(service)}`}>
+        <Button variant='contained' color='primary' component={Link} to={`/${props.service}`}>{Object.keys(service)}: {Object.values(service)}</Button>
+      </ListItem>
+    );
+  })
+  : accordion = <Typography>
+    You have not configured any endpoints for this service.
+  </Typography>;
+  return(
+    <List>
+      {accordion}
+    </List>
+  );
+}
 
 const Home = () => {
     return(
@@ -19,14 +60,22 @@ const Home = () => {
         <Container maxWidth="lg">
           <Typography variant="h1" gutterBottom>Welcome to the GA4GH Starter Kit</Typography>
           <Typography variant="h3" gutterBottom>Get Started</Typography>
-          <Typography variant="body1" gutterBottom>Click the buttons below to start using one of the GA4GH Starter Kits</Typography>
-            <div>
-              <nav>
-                <Button variant="contained" color="primary" size="large" component={Link} to='/drs'>
-                  <Typography variant="button">DRS Starter Kit</Typography>
-                </Button>
-              </nav>
-            </div>
+          <Typography variant="h5" gutterBottom>Select one of the services below to start using GA4GH Starter Kit</Typography>
+          <Box py={4}>
+            <Accordion defaultExpanded>
+              <AccordionSummary 
+              expandIcon={<ExpandMoreIcon />}
+              id='drs-accordion-header'
+              aria-controls='drs-accordion-content'>
+                <Typography variant='h5'>
+                  Data Repository Service (DRS)
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <AccordionData service='drs'/>
+              </AccordionDetails>
+            </Accordion>
+          </Box>
         </Container>
       </div>
     );
